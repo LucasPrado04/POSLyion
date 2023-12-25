2ca760debe78c69c209e1c5d2474830c5ee25879
Mauricio <contraixosh@hotmail.com>
2023-12-24 22:12:49 -0300

    Agrego botones a form Usuario

--- PosLyon/frmUsuario.Designer.cs
+++ PosLyon/frmUsuario.Designer.cs
@@ -29,23 +29,189 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
+            this.txtdni = new System.Windows.Forms.TextBox();
+            this.txtinombre_completo = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtemail = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtclave = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtconfirmar_clave = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.comborol = new System.Windows.Forms.ComboBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.Guardar = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(313, 192);
+            this.label1.Location = new System.Drawing.Point(53, 69);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(134, 25);
+            this.label1.Size = new System.Drawing.Size(26, 13);
             this.label1.TabIndex = 0;
-            this.label1.Text = "form Usuario";
+            this.label1.Text = "DNI";
+            // 
+            // txtdni
+            // 
+            this.txtdni.Location = new System.Drawing.Point(56, 85);
+            this.txtdni.Name = "txtdni";
+            this.txtdni.Size = new System.Drawing.Size(219, 20);
+            this.txtdni.TabIndex = 1;
+            this.txtdni.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // txtinombre_completo
+            // 
+            this.txtinombre_completo.Location = new System.Drawing.Point(56, 140);
+            this.txtinombre_completo.Name = "txtinombre_completo";
+            this.txtinombre_completo.Size = new System.Drawing.Size(219, 20);
+            this.txtinombre_completo.TabIndex = 3;
+            this.txtinombre_completo.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(53, 124);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(90, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Nombre completo";
+            // 
+            // txtemail
+            // 
+            this.txtemail.Location = new System.Drawing.Point(56, 191);
+            this.txtemail.Name = "txtemail";
+            this.txtemail.Size = new System.Drawing.Size(219, 20);
+            this.txtemail.TabIndex = 5;
+            this.txtemail.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(53, 175);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(155, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Dirección de correo electrónico";
+            // 
+            // txtclave
+            // 
+            this.txtclave.Location = new System.Drawing.Point(56, 248);
+            this.txtclave.Name = "txtclave";
+            this.txtclave.Size = new System.Drawing.Size(219, 20);
+            this.txtclave.TabIndex = 7;
+            this.txtclave.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(53, 232);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(61, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Contraseña";
+            // 
+            // txtconfirmar_clave
+            // 
+            this.txtconfirmar_clave.Location = new System.Drawing.Point(317, 248);
+            this.txtconfirmar_clave.Name = "txtconfirmar_clave";
+            this.txtconfirmar_clave.Size = new System.Drawing.Size(219, 20);
+            this.txtconfirmar_clave.TabIndex = 9;
+            this.txtconfirmar_clave.TextChanged += new System.EventHandler(this.textBox5_TextChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(314, 232);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(107, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Confirmar contraseña";
+            // 
+            // comborol
+            // 
+            this.comborol.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comborol.FormattingEnabled = true;
+            this.comborol.Location = new System.Drawing.Point(56, 316);
+            this.comborol.Name = "comborol";
+            this.comborol.Size = new System.Drawing.Size(139, 21);
+            this.comborol.TabIndex = 10;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(53, 297);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(23, 13);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Rol";
+            // 
+            // Guardar
+            // 
+            this.Guardar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.Guardar.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Guardar.Location = new System.Drawing.Point(90, 387);
+            this.Guardar.Name = "Guardar";
+            this.Guardar.Size = new System.Drawing.Size(150, 33);
+            this.Guardar.TabIndex = 12;
+            this.Guardar.Text = "Guardar";
+            this.Guardar.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Yellow;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(317, 387);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(150, 33);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "Editar";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Red;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(537, 387);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(150, 33);
+            this.button3.TabIndex = 14;
+            this.button3.Text = "Cancelar";
+            this.button3.UseVisualStyleBackColor = false;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(57, 9);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(151, 25);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "Nuevo usuario";
             // 
             // frmUsuario
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.Guardar);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.comborol);
+            this.Controls.Add(this.txtconfirmar_clave);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtclave);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtemail);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtinombre_completo);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtdni);
             this.Controls.Add(this.label1);
             this.Name = "frmUsuario";
             this.Text = "frmUsuario";
@@ -57,5 +223,20 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtdni;
+        private System.Windows.Forms.TextBox txtinombre_completo;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtemail;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtclave;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtconfirmar_clave;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox comborol;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button Guardar;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label7;
     }
 }
--- PosLyon/frmUsuario.cs
+++ PosLyon/frmUsuario.cs
@@ -16,5 +16,30 @@ namespace PosLyon
         {
             InitializeComponent();
         }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
